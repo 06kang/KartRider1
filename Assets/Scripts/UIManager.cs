b0f5e33ab17257143ba600af9ae627866da64cb9
77Sun77 <84917607+77Sun77@users.noreply.github.com>
2023-10-31 19:26:21 +0900

    leejunhyeok

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject Menu, Quit, X, Xbutton, penel;
+    public bool isClick = false;
     public GameObject panelLobby, panelSelect,startbtn;
 
     public void ShowSelectMap()
@@ -25,5 +27,34 @@ public class UIManager : MonoBehaviour
     public void StartMap(int mapId)
     {
         SceneManager.LoadScene(mapId);
+    }
+
+
+    public void OnClick()
+    {
+
+        Menu.SetActive(true);
+        Quit.SetActive(true);
+        Xbutton.SetActive(true);
+        penel.SetActive(true);
+        X.SetActive(false);
+    }
+
+    public void GoMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void Exit()
+    {
+        X.SetActive(true);
+        Menu.SetActive(false);
+        Quit.SetActive(false);
+        Xbutton.SetActive(false);
+        penel.SetActive(false);
     }
 }
